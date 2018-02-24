366efd5c1f0afc701aaecba27727fea3ce831cf1
Maksim Kostromin <daggerok@gmail.com>
2018-02-25 01:39:28 +0200

    spring-boot 1.5.10 gradle 4.6-rc-2

--- src/test/java/com/daggerok/spring/integration/SpringIntegrationDemoApplicationTests.java
+++ src/test/java/com/daggerok/spring/integration/SpringIntegrationDemoApplicationTests.java
@@ -2,15 +2,13 @@ package com.daggerok.spring.integration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringIntegrationDemoApplication.class)
+@SpringBootTest
 public class SpringIntegrationDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
-
+	public void contextLoads() { }
 }
